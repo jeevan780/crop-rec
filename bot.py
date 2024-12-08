5882731cd45d8acfafd93c143c220149f2c6cf30
jeevan780 <120307201+jeevan780@users.noreply.github.com>
2024-12-08 20:51:03 +0530

    Update bot.py gemini updatew

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from gtts import gTTS
 import io
 import os
 from dotenv import load_dotenv
-
+import google.generativeai as genai
 # Load data and models
 csv_file_path = 'Crop_recommendation_with_price_data1.csv'
 model_file_path = 'Crop_Recommendation_Model.pkl'
@@ -43,7 +43,17 @@ def get_farming_details(crop_name):
             f"  - Humidity: {avg_humidity:.2f}%\n"
             f"  - Rainfall: {avg_rainfall:.2f} mm"
         )
+        genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+        model = genai.GenerativeModel(model_name="gemini-1.5-flash")
+        details= str(details)
+        prompt='''translate this to kannnada and i want only kannada translation as reponse and nothing else :'''+details
+        response = model.generate_content(prompt)
+        print(response.text)
+        if response.text:
+            return response.text
         return details
+        
+        # return details
     else:
         return "Sorry, I couldn't find farming details for that crop."
 
@@ -67,6 +77,25 @@ def get_market_details(crop_name):
             f"  - Average Price Change: {avg_change:.2f}%\n"
             f"  - Recommendation: {recommendation}"
         )
+        genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+        model = genai.GenerativeModel(model_name="gemini-1.5-flash")
+        # print(details)
+        details= str(details)
+        prompt='''translate this to kannnada and i want only kannada translation as reponse and nothing else :'''+details
+        response = model.generate_content(prompt)
+        if response.text:
+            # tts = gTTS(text=message, lang='kn')
+            # audio_buffer = io.BytesIO()
+            # tts.write_to_fp(audio_buffer)
+            # audio_buffer.seek(0)
+
+            # Send voice message
+            # context.bot.send_voice(
+            #     chat_id=update.effective_chat.id,
+            #     voice=audio_buffer,
+            #     caption=message
+            # )
+            return response.text
         return details
     else:
         return "Sorry, I couldn't find market details for that crop."
@@ -149,8 +178,19 @@ async def details_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
         response = get_market_details(crop_name)
     else:
         response = "Invalid option selected."
+    
+    tts = gTTS(text=response, lang='kn')
+    audio_buffer = io.BytesIO()
+    tts.write_to_fp(audio_buffer)
+    audio_buffer.seek(0)
 
-    await query.edit_message_text(response)
+    # Send voice message
+    await context.bot.send_voice(
+        chat_id=update.effective_chat.id,
+        voice=audio_buffer,
+        caption=response
+    )
+    # await query.edit_message_text(response)
 
 # Main function to set up the bot
 def main():
